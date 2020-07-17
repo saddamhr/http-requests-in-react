cc9693088c4a9bd1f9c8104e13bc16e45a7aa34f
saddamhr <saddambubt65@gmail.com>
2020-07-17 10:32:35 +0600

    preparing the project fro routine

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,48 +3,10 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import './Blog.css';
+import Posts from './Posts/Posts'
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostID: null,
-        error: false
-    }
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Saddam'
-                    }
-                })
-                this.setState({ posts: updatedPosts })
-                // console.log(response)
-            })
-            .catch(error => {
-                this.setState({ error: true })
-                // console.log(error)
-            })
-
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostID: id })
-    }
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />
-            }
-            )
-        }
         return (
             <div className="Blog">
                 <header>
@@ -55,15 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                {/* <section>
-                    <FullPost id={this.state.selectedPostID} />
-                </section>
-                <section>
-                    <NewPost />
-                </section> */}
+                <Posts />
             </div>
         );
     }
